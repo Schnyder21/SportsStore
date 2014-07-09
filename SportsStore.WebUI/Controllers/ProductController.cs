2f6599eae2a3b9fdd3124b56cfbd1c2a2e65c456
Matt Burke <burkemd1+github@gmail.com>
2014-07-08 22:44:34 -0400

    make filtered pager work with correct total items

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -21,16 +21,16 @@ namespace SportsStore.WebUI.Controllers
 
         public ViewResult List(string category, int page = 1)
         {
+            var productsQuery = repository.Products
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductID);
+                    
             var model = new ProductsListViewModel() {
-                Products = repository.Products
-                    .Where( p => category == null || p.Category == category)
-                    .OrderBy(p => p.ProductID)
-                    .Skip((page - 1) * PageSize)
-                    .Take(PageSize),
+                Products = productsQuery.Skip((page - 1) * PageSize).Take(PageSize),
                 PagingInfo = new PagingInfo {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = productsQuery.Count()
                 },
                 CurrentCategory = category
             };

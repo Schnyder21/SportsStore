8e9483b2221b933508848834a23d8fe97b7aae9f
Matt Burke <burkemd1+github@gmail.com>
2014-07-08 23:10:21 -0400

    nav bar

---
+++ SportsStore.UnitTests/Controllers/NavControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests.Controllers
+{
+    [TestFixture]
+    public class NavControllerTests
+    {
+        [Test]
+        public void CanCreateCategories()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductID = 2, Name = "P2", Category = "Apples" },
+                new Product { ProductID = 3, Name = "P3", Category = "Plums" },
+                new Product { ProductID = 4, Name = "P4", Category = "Oranges" }
+            }.AsQueryable());
+
+            var controller = new NavController(mock.Object);
+
+            var results = ((IEnumerable<string>)controller.Menu().Model).ToArray();
+
+            Assert.That(results, Is.EqualTo(new string[] { "Apples", "Oranges", "Plums" }));
+        }
+    }
+}

c9b6ce538af24fe73fd4d3149a8f376900d1de8a
Matt Burke <burkemd1+github@gmail.com>
2014-07-09 21:47:32 -0400

    Cart Index view
    and styles!

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,15 @@ namespace SportsStore.WebUI.Controllers
         public CartController(IProductRepository repository)
         {
             this.repository = repository;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = Cart,
+                ReturnUrl = returnUrl
+            });
         }
 
         public RedirectToRouteResult AddToCart(int productID, string returnUrl)

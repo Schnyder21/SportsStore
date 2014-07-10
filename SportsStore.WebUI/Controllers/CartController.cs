f2204a55de2a3898265f7e24390948e870cefcdc
Matt Burke <burkemd1+github@gmail.com>
2014-07-09 21:35:06 -0400

    CartController

---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+        {
+            var product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            if (product != null)
+            {
+                Cart.AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart.RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart Cart
+        {
+            get
+            {
+                var cart = (Cart)Session["Cart"];
+                if (cart == null)
+                {
+                    cart = new Cart();
+                    Session["Cart"] = cart;
+                }
+                return cart;
+            }
+        }
+    }
+}

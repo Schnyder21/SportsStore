e986b666e069c3f2ea8175fde7cc47ddc17442af
Matt Burke <burkemd1+github@gmail.com>
2014-07-09 22:00:16 -0400

    Custom model binding for Cart

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -18,51 +18,38 @@ namespace SportsStore.WebUI.Controllers
             this.repository = repository;
         }
 
-        public ViewResult Index(string returnUrl)
+        public ViewResult Index(Cart cart, string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = Cart,
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
 
-        public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+        public RedirectToRouteResult AddToCart(Cart cart, int productID, string returnUrl)
         {
             var product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
 
             if (product != null)
             {
-                Cart.AddItem(product, 1);
+                cart.AddItem(product, 1);
             }
 
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
         {
             var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
             {
-                Cart.RemoveLine(product);
+                cart.RemoveLine(product);
             }
 
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        private Cart Cart
-        {
-            get
-            {
-                var cart = (Cart)Session["Cart"];
-                if (cart == null)
-                {
-                    cart = new Cart();
-                    Session["Cart"] = cart;
-                }
-                return cart;
-            }
-        }
     }
 }
